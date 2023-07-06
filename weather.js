c45bbe7bfb1d6eba1ce976193233d170d8b22ceb
shrutia003 <shrutiasolkar01@gmail.com>
2023-07-06 18:32:44 -0500

    JS Script updated

---
+++ weather.js
@@ -0,0 +1,44 @@
+const api = '7dfc5bee1219dea6d3fe817c75a827be'; //API key from https://openweathermap.org/
+
+const iconImg = document.getElementById('weather-icon-png-5');
+const loc = document.querySelector('#location');
+const tempC = document.querySelector('.c');
+const tempF = document.querySelector('.f');
+const desc = document.querySelector('.desc');
+const sunriseDOM = document.querySelector('.sunrise');
+const sunsetDOM = document.querySelector('.sunset');
+
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position) => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const base = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric';
+
+            fetch(base)
+              .then((response) => {
+                return response.json();
+              })
+              .then((data) => {
+                const { temp, feels_like } = data.main;
+                const place = data.name;
+                const { description, icon } = data.weather[0];
+                const { sunrise, sunset } = data.sys;
+                const iconURL = 'http://openweathermap.org/img/wn/${icon}@2x.png';
+                const fahrenheit = (temp * 9) / 5 + 32;
+                const sunriseGMT = new Date(sunrise * 1000);
+                const sunsetGMT = new Date(sunset * 1000);
+
+                iconImg.src = iconURL;
+                loc.textContent = '${place}';
+                desc.textContent = '${description}';
+                tempC.textContent = '${temp.toFixed(2)} °C';
+                tempF.textContent = '${fahrenheit.toFixed(2)} °F';
+                sunriseDOM.textContent = '${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}';
+                sunsetDOM.textContent = '${sunsetGMT.toLocaleDataString()}, ${sunsetGMT.toLocaleTimeString()}';
+              });
+        });
+    }
+});
